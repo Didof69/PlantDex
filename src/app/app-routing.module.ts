03aec9aebf9e52c4004d38dbe7fefe656453a2cd
Lydie Chevalier <didof69@hotmail.fr>
2023-07-10 16:48:36 +0200

    3.1 - avec correction du menu burger de la navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageMyPlantsComponent } from './pages/page-my-plants/page-my-plants.component';
+import { PageAdminComponent } from './pages/page-admin/page-admin.component';
+
+const routes: Routes = [
+  { path: '', component: PageHomeComponent },
+  { path: 'my-plants', component: PageMyPlantsComponent },
+  { path: 'admin', component: PageAdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
