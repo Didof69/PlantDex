1e458429cf2569490d4f6a565879feb381596c2b
Lydie Chevalier <didof69@hotmail.fr>
2023-07-11 16:04:52 +0200

    4 - affichage plant + service

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlantService } from '../../services/plant.service';
+import { Plant } from 'src/app/models/plant';
 
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.component.html',
-  styleUrls: ['./page-home.component.css']
+  styleUrls: ['./page-home.component.css'],
 })
-export class PageHomeComponent {
+export class PageHomeComponent implements OnInit {
+  plantsToDisplay: Plant[] = [];
+  constructor(private plantService: PlantService) {}
 
+  ngOnInit() {
+    this.plantService.getPlants().subscribe((plants) => {
+      this.plantsToDisplay = plants;
+      console.log(this.plantsToDisplay);
+    });
+  }
 }
