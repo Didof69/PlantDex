ab76ffadbcfde2318314ed2d698a5cadecf777ef
Lydie Chevalier <didof69@hotmail.fr>
2023-07-12 15:37:38 +0200

    appel de card dans plant list avec ngfor

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,18 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Plant } from 'src/app/models/plant';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
- plant: Plant = {
-      id: 952438,
-      nom: "Anthurium, pot D12cm",
-      soleil: "moyen",
-      arrosage: 2,
-      categorie: "plantes fleuries",
-      image: "https://images.truffaut.com/media/catalog/product/cdn:///Articles/jpg/0952000/952438_003.jpg"
-    }
+  @Input() plant!: Plant;
 }
