a95822a60922c4b61f483ce8f25f442879606d6f
Lydie Chevalier <didof69@hotmail.fr>
2023-07-13 14:12:42 +0200

    creation method onCheckCategory() / recupération valeur et checked

--- src/app/components/filter-bar/filter-bar.component.ts
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -6,5 +6,15 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./filter-bar.component.css'],
 })
 export class FilterBarComponent {
-  @Input() tabCategorie!: Array<string>;
+  @Input() tabCategorie!: Array<string>;  
+
+  onCheckCategory(e:Event) {
+    console.log(e);
+
+    //recupérer la valeur de la checkbox et son etat
+    const target = e.target as HTMLInputElement;
+    console.log("valeur de la checkbox " + target.value);
+    console.log('cochée?' + target.checked);
+    
+  }
 }
