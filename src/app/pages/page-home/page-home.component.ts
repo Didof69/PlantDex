6cd5410652acacff79fc570018b5bc9766cac5f7
Lydie Chevalier <didof69@hotmail.fr>
2023-07-13 09:38:58 +0200

    recuperation categorie + affichage dans filterbar avec checkbox

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlantService } from '../../services/plant.service';
 import { Plant } from 'src/app/models/plant';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-page-home',
@@ -9,12 +10,40 @@ import { Plant } from 'src/app/models/plant';
 })
 export class PageHomeComponent implements OnInit {
   plantsToDisplay: Plant[] = [];
-  constructor(private plantService: PlantService) {}
+  constructor(private plantService: PlantService) { }
 
   ngOnInit() {
     this.plantService.getPlants().subscribe((plants) => {
       this.plantsToDisplay = plants;
       console.log(this.plantsToDisplay);
+      this.recupererCategories()
     });
   }
+
+  recupererCategories():string[] {
+    let tabCategorie: string[] = [];
+    for (const plant of this.plantsToDisplay) {
+      tabCategorie.push(plant.categorie);
+    }
+    console.log(tabCategorie);
+
+    let tableauAvecIndices = tabCategorie.map((valeur, index) => {
+      return { valeur: valeur, index: index };
+    });
+    console.log(tableauAvecIndices);
+
+    let tableauSansDoublons = tableauAvecIndices.filter((objet, index) => {
+      return (
+        tableauAvecIndices.findIndex((item) => item.valeur === objet.valeur) ===
+        index
+      );
+    });
+    console.log(tableauSansDoublons);
+    let tabCategorieUnique = tableauSansDoublons.map((objet) => objet.valeur);
+
+    return tabCategorieUnique
+  }
 }
+
+
+
