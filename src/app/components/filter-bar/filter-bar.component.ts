d22108825a5cf32a88a606b9266dfa1c82be0193
Lydie Chevalier <didof69@hotmail.fr>
2023-07-13 15:27:27 +0200

    ajout et suppression de categorie dans filtre

--- src/app/components/filter-bar/filter-bar.component.ts
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -6,15 +6,32 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./filter-bar.component.css'],
 })
 export class FilterBarComponent {
-  @Input() tabCategorie!: Array<string>;  
+  @Input() tabCategorie!: Array<string>;
+  categorieFiltre: string[] = [];
 
-  onCheckCategory(e:Event) {
+  onCheckCategory(e: Event) {
     console.log(e);
-
     //recupérer la valeur de la checkbox et son etat
     const target = e.target as HTMLInputElement;
-    console.log("valeur de la checkbox " + target.value);
-    console.log('cochée?' + target.checked);
-    
+    // console.log('valeur de la checkbox ' + target.value);
+    // console.log('cochée?' + target.checked);
+
+    if (target.checked) {
+      this.categorieFiltre.push(target.value);
+      // console.log(this.categorieFiltre);
+    }
+
+    if (!target.checked) {
+      if (this.categorieFiltre.includes(target.value)) {
+        this.categorieFiltre = this.categorieFiltre.filter(
+          (e) => e != target.value
+        );
+      } else {
+        this.categorieFiltre.push(target.value);
+      }
+      // console.log(this.categorieFiltre);
+    }
+
+    console.log(this.categorieFiltre);
   }
 }
